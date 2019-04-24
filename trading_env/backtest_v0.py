0831bac52518407920514757750600274d9261ba
mimmic <mimmicbox@gmail.com>
2019-04-24 23:53:59 +0900

    backtest

--- trading_env/backtest_v0.py
+++ trading_env/backtest_v0.py
@@ -40,11 +40,14 @@ class _Broker(metaclass=ABCMeta):
         self._close = self._data.Adj_Close[self._current] if use_adj_close \
                         else self._data.Close[self._current]
 
+        self._next_close = self._data.Adj_Close[self._current + 1] if use_adj_close \
+                        else self._data.Close[self._current + 1]
+
         self._order = _Order
 
+    @abstractmethod
     def reset(self):
-        self._position = 0
-        self._order.cancel()
+        pass
 
     @abstractmethod
     def buy(self, price=None):
@@ -56,11 +59,20 @@ class _Broker(metaclass=ABCMeta):
 
     @property
     def pos(self):
-        return self._position 
+        return self._position
+    
+    @pos.setter
+    def pos(self, position):
+        self._position = position
+        return self
 
     @property
     def _current(self):
         return self._trading_env._broker_current_day
+
+    @property
+    def next_close(self):
+        return self._next_close
 
     @property 
     def last_close(self):
@@ -94,6 +106,11 @@ class _Broker_Ratio(_Broker):
         super(_Broker_Ratio, self).__init__(*args, **kwargs)
         self._order = _Order_Ratio(self)
 
+    def reset(self):
+        self._position = 0
+        self._order.cancel()
+
+
     def buy(self, price):
         self._order._update(price, is_long=True)
     
@@ -105,23 +122,28 @@ ENV_NAME_FOR_CLASS = {
     'ratio' : _Broker_Ratio
 }
 
-class Trading_env:
+class Trading_env():
     class _Log:
-        def __init__(self):
-            pass
+        def __init__(self, lenght):
+            self.length = length
 
         def reset(self):
-            self.return_per_symbol = []
+            self.returns_diff      = []
+            self.returns_ratio     = []
             self.returns           = []
-            self.positions         = []
+            self.entry_cashs       = []
+            self.entrys            = []
+            self.longs             = []
 
+        def __repr__(self):
+            pass
 
     def __init__(self, env, data, cash, fee, leverage=1.0, adj_close=None):
         assert 0 < cash, "cash shosuld be >0, is {}".format(cash)
         assert 0 <= fee < .1, "commission should be between 0-10%, is {}".format(fee)
         # assert 0 < margin <= 1, "margin should be between 0 and 1, is {}".format(margin)
         
-        self._d = 0
+        self._lenght = data.length
         self._symbols = data.symbols
         self._features = data.features
         
@@ -150,21 +172,74 @@ class Trading_env:
         self._cash = self.__cash
         self._log.reset()
 
-        for broker in _brokers_generater:
-            broker.reset()
-
         if init_state is None:
             init_state = [0] * len(self._symbols)
+        for broker in self._brokers_generater:
+            broker.reset()
         self.portfolio_pos(init_state)
 
 
     def step(self):
-        pass
+        next_d = self._d + 1
+        done = True if next_d == self.lenght else False
+        
+
+        returns_diff   = []
+        returns_ratio  = []
+        entry_cashs    = []
+        entrys         = []
+        longs          = []
+
+        for broker in self._brokers_generater:
+            cur_pos = broker.pos
+            ratio = broker.next_close / broker.last_close
+            is_long = broker._order.is_long
+            entry = broker._order.entry
+
+            entry = entry if entry else 0
+
+            if is_long is None:
+                entry_cash = 0
+                next_pos = cur_pos * ratio
+            elif is_long:
+                entry_cash = -entry * (1 + self._fee)
+                nextr_cash = self._cash - entry * entry_cash
+                next_pos = (cur_pos + entry) * ratio
+            else:
+                entry_cash = entry * (1 - self._fee)
+                next_cash = self._cash + entry * entry_cash
+                next_pos = (cur_pos - entry) * ratio
+            next_pos_diff = next_pos - cur_pos
+            next_pos_ratio = (next_cash + next_pos) / (self._cash + cur_pos)
+
+
+            returns_diff.append(next_pos_diff)
+            returns_ratio.append(next_pos_ratio)
+            entry_cashs.append(entry_cash)
+            entrys.append(entry)
+            longs.append(is_long)
+
+
+            self._order.pos = next_pos
+            self._order.cancel()
+
+        self._log.returns_diff.append(returns_diff)
+        self._log.returns_ratio.append(returns_ratio)
+        self._log.entry_cashs.append(entry_cashs)
+        self._log.entrys.append(entrys)
+        self._log.longs.append(longs)
+
+        self._cash = self._cash + np.sum(entry_cashs)
+        self._d = self._d + 1
+
+        return self.get_brokers_close, returns_ratio, done, self._log
 
     def action(self, portfolio, cash_per=0.0, lt=0.0, st=0.0):
         self.portfolio_pos(portfolio, cash_per=cash_per, lt=lt, st=st)
         
-    
+    def get_brokers_close(self):
+        return [ broker.last_close for broker in self._brokers_generater ]
+
     @property
     def equity(self):
         return self._cash + np.sum([broker.pos for broker in self._brokers_generater])
@@ -189,7 +264,7 @@ class Trading_env:
         assert self.__is_portfolio_format(portfolio) 
         
         ratio_sum = np.sum(portfolio) + cash_per
-        portfolio = [self._cash * p / ratio_sum for p in portfolio]
+        self.portfolio = [self._cash * p / ratio_sum for p in portfolio]
         cash =  self._cash * cash_per / ratio_sum
 
         brokers_cur_pos = [ broker.pos for broker in self._brokers_generater ]
@@ -200,5 +275,3 @@ class Trading_env:
                 broker.buy(price=pos)
             elif pos < st:
                 broker.sell(price=pos)
-            else:
-                cash += pos
